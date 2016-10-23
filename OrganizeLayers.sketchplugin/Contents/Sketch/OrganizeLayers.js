012caa89f5e8bea0e9539947fd66695b797dbe01
David Keegan <git@davidkeegan.com>
2016-10-23 00:58:53 -0700

    trying to perfect the plugin

--- OrganizeLayers.sketchplugin/Contents/Sketch/OrganizeLayers.js
+++ OrganizeLayers.sketchplugin/Contents/Sketch/OrganizeLayers.js
@@ -1,6 +1,6 @@
-function moveForward(layer, amount) {
+function moveLayer(layer, amount) {
     for (var i = 0; i < amount; ++i) {
-        layer.moveForward()
+        layer.moveBackward();
     }
 }
 
@@ -21,9 +21,10 @@ function organizeLayers(prefix, layers) {
     });
 
     var indexOffset = sortedLayers[0].index;
-    sortedLayers.forEach(function(layer, index){
-        layer.name = prefix+" "+(index+1);
-        moveForward(layer, indexOffset-layer.index-index);
+    sortedLayers.reverse().forEach(function(layer, index) {
+        var moveOffset = layer.index-index-indexOffset;
+        layer.name = prefix+" "+(sortedLayers.length-index);
+        moveLayer(layer, moveOffset);
     });
 }
 
@@ -38,6 +39,7 @@ var onRun = function (context) {
         var prefix = sketch.getStringFromUser("Prefix name", "");
         organizeLayers(prefix, selection.nativeLayers);
     }
+    sketch.message("Layers organized!");
 }
 
 // Tests
@@ -47,18 +49,31 @@ var onRun = function (context) {
 // make sure order and naming is what we expect
 
 function testOrganizeLayers1(tester) {
+    var sketch = context.api();
     var page = sketch.selectedDocument.selectedPage;
     var parent = page.newGroup();
     var prefix = "Table Row";
 
+    var layers = [];
     var bottomLayer = parent.newGroup({"name":"Bottom Group"});
-    var layer3 = parent.newGroup({"name":"Group 3", "frame": new Rectangle(0, 88, 100, 44)});
-    var layer2 = parent.newGroup({"name":"Group 2", "frame": new Rectangle(0, 44, 100, 44)});
-    var layer4 = parent.newGroup({"name":"Group 4", "frame": new Rectangle(0, 132, 100, 44)});
     var layer1 = parent.newGroup({"name":"Group 1", "frame": new Rectangle(0, 0, 100, 44)});
+    layers.push(layer1);
+    var layer4 = parent.newGroup({"name":"Group 4", "frame": new Rectangle(0, 132, 100, 44)});
+    layers.push(layer4);
+    var layer2 = parent.newGroup({"name":"Group 2", "frame": new Rectangle(0, 44, 100, 44)});
+    layers.push(layer2);
+    var layer3 = parent.newGroup({"name":"Group 3", "frame": new Rectangle(0, 88, 100, 44)});
+    layers.push(layer3);
     var topLayer = parent.newGroup({"name":"Top Group"});
     
-    organizeLayers(prefix, [layer1, layer2, layer3, layer4]);
+    tester.assertEqual(topLayer.index, 5);
+    tester.assertEqual(layer3.index, 4);
+    tester.assertEqual(layer2.index, 3);
+    tester.assertEqual(layer4.index, 2);
+    tester.assertEqual(layer1.index, 1);
+    tester.assertEqual(bottomLayer.index, 0);
+
+    organizeLayers(prefix, layers);
 
     tester.assertEqual(topLayer.index, 5);
 
@@ -90,5 +105,6 @@ var tests = {
 }
 
 function runTests() {
+    var sketch = context.api();
     log(sketch.tester.runUnitTests(tests));
 }
