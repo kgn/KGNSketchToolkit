1b0f5c3c950d432d1ca3ba9f18b0244e6c1c21cc
David Keegan <git@davidkeegan.com>
2016-10-27 00:49:16 -0700

    Start of the export page plugin

---
+++ ExportPage.sketchplugin/Contents/Sketch/ExportPage.js
@@ -0,0 +1,93 @@
+// Commands
+
+function exportPage(page, labelOffset, exportPadding) {
+    var group, groupName = "Export Page Group";
+    var slice, sliceName = "Export Group Slice";
+
+    var artboards = [];
+    page.iterate(function(layer) {
+        if (layer.name == groupName) {
+            group = layer;
+            return;
+        }
+        if (layer.isArtboard) {
+            artboards.push(layer);
+        }
+    });
+
+    if (group === undefined) {
+        group = page.newGroup({"name": groupName});
+    } else {
+        var layers = [];
+        group.iterate(function(layer) {
+            if (layer.name == sliceName) {
+                slice = layer;
+                return;
+            }
+            layers.push(layer);
+        });
+        layers.forEach(function(layer) {
+            layer.remove();
+        });
+        group.frame = new Rectangle();
+    }
+
+    var sliceFrame;
+    artboards.forEach(function(artboard) {
+        var label = group.newText({"name": artboard.name});
+        label.systemFontSize = 20;
+        label.text = artboard.name;
+        var labelFrame = label.frame;
+        labelFrame.x = artboard.frame.x;
+        labelFrame.y = artboard.frame.y-labelFrame.height-labelOffset;
+        label.frame = labelFrame;
+
+        if (sliceFrame === undefined) {
+            sliceFrame = label.frame;
+        }
+
+        if (labelFrame.x < sliceFrame.x) { sliceFrame.x = labelFrame.x }
+        if (labelFrame.y < sliceFrame.y) { sliceFrame.y = labelFrame.y }
+
+        if (labelFrame.maxX > sliceFrame.maxX) { sliceFrame.width = labelFrame.maxX-sliceFrame.x }
+        if (artboard.frame.maxX > sliceFrame.maxX) { sliceFrame.width = artboard.frame.maxX-sliceFrame.x }
+
+        if (artboard.frame.maxY > sliceFrame.maxY) { sliceFrame.height = artboard.frame.maxY-sliceFrame.y }
+    });
+    sliceFrame.inset(-exportPadding, -exportPadding);
+    if (slice === undefined) {
+        slice = group.newSlice({"name": sliceName});
+    }
+    slice.frame = sliceFrame;
+    slice.moveToFront();
+
+    group.adjustToFit();
+    group.lock = true;
+
+    slice.select();    
+}
+
+function main(context) {
+    var sketch = context.api();
+    var document = sketch.selectedDocument;
+    var page = document.selectedPage;
+    var labelOffset = 10, exportPadding = 10;
+    exportPage(page, labelOffset, exportPadding);
+}
+
+// Tests
+
+var tests = {
+    "suites" : {
+        "Export Page": {
+            "tests" : {
+
+            }
+        }
+    }
+}
+
+function runTests() {
+    log(context.api().tester.runUnitTests(tests));
+}
+// runTests()
--- README.md
+++ README.md
@@ -6,8 +6,31 @@ These plugins are very much a work in progress, the plan is for them to only use
 Renames and orders layers in the layer list based on their screen layout.
 ![Organize Layers](OrganizeLayers.gif)
 
-## Export Page - TBD
+### TODO
+- [ ] Improve plugin for selections that span differnt groups
+- [ ] Custom user interface
+
+## Export Page
 Export all the artboards on the page to an image with artboard titles.
+
+### TODO
+- [ ] [Automate Export](https://github.com/BohemianCoding/SketchAPI/pull/9)
+- [ ] Custom user interface(title options, output options)
+
+## Mobile Export - TBD
+Export assets for all mobile sizes with platform naming conventions.
+
+### iOS
+- Naming: "Image Name" -> "ImageName"
+- Sizes: @1x, @2x, @3x
+
+### Android
+- Naming: "Image Name" -> "image_name"
+- Sizes: ldpi(0.75x), mdpi(1x), hdpi(1.5x), xhdpi(2x), xxhdpi(3x), xxxhdpi(4x)
+
+### TODO
+- [ ] Build it ;)
+- [ ] [Automate Export](https://github.com/BohemianCoding/SketchAPI/pull/9)
 
 ## Resources:
 - http://developer.sketchapp.com/introduction/plugin-bundles/
